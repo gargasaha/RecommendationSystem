201ace74b1fadc8cd484ed56d30bae9c0fdc3fd3
Garga <sahagarga000@gmail.com>
2025-07-26 00:50:52 +0530

    updated database

--- app.py
+++ app.py
@@ -14,6 +14,11 @@ train_data = pd.read_csv("models/clean_data.csv")
 
 app.secret_key = "alskdjfwoeieiurlskdjfslkdjf"
 
+<<<<<<< HEAD
+=======
+import pyodbc
+
+>>>>>>> 1161480 (updated database)
 def get_db_connection():
     return pyodbc.connect(
         'DRIVER={ODBC Driver 17 for SQL Server};'
