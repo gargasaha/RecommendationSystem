6d786efd5d33b48aad68d10b0d6a705cf1d40358
Garga <sahagarga000@gmail.com>
2025-07-26 11:59:19 +0530

    updated database

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, render_template, session,redirect
 import pandas as pd
 import random
 import mysql.connector
-import pyodbc
 from mysql.connector import Error
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -14,18 +13,14 @@ train_data = pd.read_csv("models/clean_data.csv")
 
 app.secret_key = "alskdjfwoeieiurlskdjfslkdjf"
 
-
 def get_db_connection():
-    return pyodbc.connect(
-        'DRIVER={ODBC Driver 17 for SQL Server};'
-        'SERVER=FinalyearProject.mssql.somee.com;'
-        'DATABASE=FinalyearProject;'
-        'UID=arko_SQLLogin_1;'
-        'PWD=6wpm9uy38j;'
-        'TrustServerCertificate=Yes;'
-        'Persist Security Info=False;'
-        'Packet Size=4096;'
+    return mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='9932',
+        database='ProductRecommendationSystem'
     )
+
 def truncate(text, length):
     if len(text) > length:
         return text[:length] + "..."
